c3b90a29ab73ffdbeb7afa2709792a8858ed4e7f
Douglas Marra <douglasmsi@gmail.com>
2018-08-11 00:00:55 -0300

    First commit

---
+++ src/main/java/br/com/dmsec/idup/payload/UploadHashsResponse.java
@@ -0,0 +1,5 @@
+package br.com.dmsec.idup.payload;
+
+public class UploadHashsResponse {
+
+}
--- src/test/java/br/com/dmsec/idup/AppTests.java
+++ src/test/java/br/com/dmsec/idup/AppTests.java
@@ -1,4 +1,4 @@
-package com.example.filedemo;
+package br.com.dmsec.idup;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FileDemoApplicationTests {
+public class AppTests {
 
 	@Test
 	public void contextLoads() {
